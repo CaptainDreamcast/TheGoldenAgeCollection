7f7e95f2341d01f2c5e54cc577cf55370cc9bdf2
Josh Tari <josh.tari@dogmacompany.com>
2017-10-16 00:00:43 +0200

    [Feature] add prism game skeleton

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <tari/framerateselectscreen.h>
+#include <tari/pvr.h>
+#include <tari/physics.h>
+#include <tari/file.h>
+#include <tari/drawing.h>
+#include <tari/log.h>
+#include <tari/wrapper.h>
+#include <tari/system.h>
+#include <tari/stagehandler.h>
+#include <tari/logoscreen.h>
+
+#ifdef DREAMCAST
+KOS_INIT_FLAGS(INIT_DEFAULT);
+
+extern uint8 romdisk[];
+KOS_INIT_ROMDISK(romdisk);
+
+#endif
+
+
+void exitGame() {
+	shutdownTariWrapper();
+
+#ifdef DEVELOP
+	abortSystem();
+#else
+	returnToMenu();
+#endif
+}
+
+void setMainFileSystem() {
+#ifdef DEVELOP
+	setFileSystem("/pc");
+#else
+	setFileSystem("/cd");
+#endif
+}
+
+int main(int argc, char** argv) {
+	(void)argc;
+	(void)argv;
+
+	setGameName("DOGMA: THE GOLDEN AGE");
+	setScreenSize(640, 480);
+	
+	initTariWrapperWithDefaultFlags();
+	setFont("$/rd/fonts/segoe.hdr", "$/rd/fonts/segoe.pkg");
+
+	logg("Check framerate");
+	FramerateSelectReturnType framerateReturnType = selectFramerate();
+	if (framerateReturnType == FRAMERATE_SCREEN_RETURN_ABORT) {
+		exitGame();
+	}
+
+	setMainFileSystem();
+	
+	setScreenAfterWrapperLogoScreen(getLogoScreenFromWrapper());
+	startScreenHandling(getLogoScreenFromWrapper());
+
+	exitGame();
+
+	return 0;
+}
+
+
